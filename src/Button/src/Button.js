97ed3636360385a31fedff8a3782d716e0f7bd2e
Prakash Naidu <prakashnaidu@Prakashs-MacBook-Pro.local>
2022-03-04 16:35:10 +0530

    removed .npmrc

--- src/Button/src/Button.js
+++ src/Button/src/Button.js
@@ -5,7 +5,7 @@ export default function Button(props) {
 		<button
 			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 			type="button"
-			onClick={props.onClick}
+			{...props}
 		>
 			{props.children}
 		</button>
