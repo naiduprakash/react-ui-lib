6aed4baf3871d9af1d95acbb554493e595aaa677
Prakash Naidu <prakashnaidu@Prakashs-MacBook-Pro.local>
2022-03-04 16:43:34 +0530

    removed .npmrc

--- src/Button/src/Button.js
+++ src/Button/src/Button.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Button(props) {
 	return (
 		<button
-			className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+			className="bg-blue-500  hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
 			type="button"
 			{...props}
 		>
