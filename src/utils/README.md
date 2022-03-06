b6847c9c62bb59c8e7eed665aaf2f2c1a61e2234
Prakash Naidu <prakashnaidu@Prakashs-MacBook-Pro.local>
2022-03-07 03:38:33 +0530

    added button variant

---
+++ src/utils/README.md
@@ -0,0 +1,5 @@
+# utils component
+
+## Usage
+
+Import component with: `import utils from '@react-ui-lib/utils';`
---
+++ src/utils/src/index.js
@@ -0,0 +1,7 @@
+import clsx from "classnames";
+import { twMerge } from "tailwind-merge";
+
+export const __DEV__ = process.env.NODE_ENV !== "production";
+export const cx = (...args) => {
+	return twMerge(clsx(...args));
+};
